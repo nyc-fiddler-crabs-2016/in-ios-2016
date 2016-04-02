d4ae90818dc517e1500a105545e40b5b130a718b
Ryan F. Salerno <ryan@salerno.co>
2016-04-02 10:49:03 -0400

    Adding new conversation controller.

--- In/NewConversationController.swift
+++ In/NewConversationController.swift
@@ -8,25 +8,22 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class NewConversationController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
-    @IBAction func navToNewConversationForm() {
-        self.performSegueWithIdentifier("toNewConversationForm", sender: self)
+    
+    
+    @IBAction func backHome() {
+        self.performSegueWithIdentifier("backHome", sender: self)
     }
     
     
-    
-    
 }
-
