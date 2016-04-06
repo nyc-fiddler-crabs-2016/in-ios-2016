1174dd20e3eeaaaaae58d255c2f2befbf2e5cec6
PeteStrat <Stradakis@gmail.com>
2016-04-05 22:23:10 -0400

    Add a 'most recent date' field to conversations class.

--- In/ConversationTableViewController.swift
+++ In/ConversationTableViewController.swift
@@ -57,9 +57,19 @@ class ConversationTableViewController: UITableViewController {
                         let name = snapshot.value["name"] as! String
                         let owner = snapshot.value["owner"] as! String
                         let participants = snapshot.value["participants"] as! NSArray
+                        var lastMessage: NSDate!
+                        
+                        snapshot.ref.childByAppendingPath("messages").queryLimitedToLast(1).observeEventType(.Value, withBlock: {snapshot in
+                            let dateFormatter = NSDateFormatter()
+                            var dateAsString = snapshot.value["date"] as! String
+                            dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss Z"
+                            let newDate = dateFormatter.dateFromString(dateAsString) as! NSDate!
+                            lastMessage = newDate
+                            })
+                        
 //                        let mostRecentMessage = self.convsersationRef.queryOrderedByChild("messages").observeEventType(.Value, withBlock{ snapshot in
 //                            )
-                        let conversation = Conversation(name: name, date: "Manana", owner: owner, conversationId: snapshot.key, participants: participants)
+                        let conversation = Conversation(name: name, date: "Manana", owner: owner, conversationId: snapshot.key, participants: participants, mostRecentMessage: lastMessage)
                         self.conversations.append(conversation)
                         dispatch_async(dispatch_get_main_queue()) {
                             self.tableView.reloadData()
@@ -149,7 +159,7 @@ class ConversationTableViewController: UITableViewController {
                 }
                 firebaseTarget.childByAppendingPath("participants").observeEventType(.ChildAdded, withBlock: { snapshot in
                     if snapshot.value as! String == self.myPhoneNumber{
-                        print(snapshot.ref.setValue(nil))
+//                        print(snapshot.ref.setValue(nil))
                     }
                     
                 })
